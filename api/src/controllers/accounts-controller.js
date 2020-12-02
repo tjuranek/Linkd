baf6dd54564112e116c5140f74d14e6ce9a842dd
tjuranek <thomas@juranek.com>
2020-12-01 21:08:09 -0600

    add express validator to account routes

--- api/src/controllers/accounts-controller.js
+++ api/src/controllers/accounts-controller.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import validator from 'express-validator';
 import { AccountsService } from '../services/accounts-service.js';
 import { PasswordService } from '../services/password-service.js';
 import { TokensService } from '../services/tokens-service.js';
@@ -9,33 +10,55 @@ const accountsService = new AccountsService();
 const passwordService = new PasswordService();
 const tokensService = new TokensService();
 
+const { body, validationResult } = validator;
+
 /**
  * Creates a new user from a given email and password, returning a newly generated access token for the user.
  */
-AccountsRouter.post('/register', async (req, res) => {
-	const { email, password } = req.body;
-	const hash = await passwordService.generatePassword(password);
-	const { _id } = await accountsService.createAccount(email, hash);
-	const token = await tokensService.generateToken({ id: _id });
+AccountsRouter.post(
+	'/register',
+	[body('email').isEmail(), body('password').isLength({ min: 8, max: 64 })],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-	return res.status(200).json({ token });
-});
+		const { email, password } = req.body;
+		const hash = await passwordService.generatePassword(password);
+		const { _id } = await accountsService.createAccount(email, hash);
+		const token = await tokensService.generateToken({ id: _id });
+
+		return res.status(200).json({ token });
+	}
+);
 
 /**
  * Generates and returns a new access token for a user given valid credentials.
  */
-AccountsRouter.post('/login', async (req, res) => {
-	const { email, password } = req.body;
-	const account = await accountsService.getAccount(email);
-	const isValidPassword = await passwordService.verifyPassword(
-		password,
-		account.password
-	);
+AccountsRouter.post(
+	'/login',
+	[body('email').isEmail(), body('password').isLength({ min: 8, max: 64 })],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-	if (isValidPassword) {
-		const token = await tokensService.generateToken({ id: account._id });
-		return res.status(200).json({ token });
+		const { email, password } = req.body;
+		const account = await accountsService.getAccount(email);
+		const isValidPassword = await passwordService.verifyPassword(
+			password,
+			account.password
+		);
+
+		if (isValidPassword) {
+			const token = await tokensService.generateToken({
+				id: account._id
+			});
+			return res.status(200).json({ token });
+		}
+
+		return res.sendStatus(400);
 	}
-
-	return res.sendStatus(400);
-});
+);
