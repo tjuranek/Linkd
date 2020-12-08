d25cc99a97d79ba8dc720d73a31f11c897ef35d0
tjuranek <thomas@juranek.com>
2020-12-07 23:44:42 -0600

    add material ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Heading } from './components/heading';
 
 const App = () => {
-	return <h1>Testing!</h1>;
+	return (
+		<div>
+			<Heading />
+			<h1>Testing!</h1>
+		</div>
+	);
 };
 
 render(<App />, document.getElementById('root'));
