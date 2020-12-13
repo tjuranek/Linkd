f36fcc173f02b6b085092b7ef08d2c4a7655ced4
tjuranek <thomas@juranek.com>
2020-12-13 17:00:22 -0600

    add cors to api

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { connectToDatabase } from './src/db/connection.js';
 import { MainRouter } from './src/controllers/router.js';
 import { logger } from './src/middleware/logger-middleware.js';
 
 // setup express app
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(logger);
 app.use(MainRouter);
