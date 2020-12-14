7eb10d04958127440ee85e74cd657e625291a963
tjuranek <thomas@juranek.com>
2020-12-14 17:22:13 -0600

    add sourcemap to webpack config

--- client/src/components/heading.js
+++ client/src/components/heading.js
@@ -6,17 +6,20 @@ export const Heading = () => {
 	const [token, setToken] = useState('');
 
 	const handleClick = async () => {
-		const reponse = await post('http://localhost:1234/accounts/login', {
+		debugger;
+
+		alert('test');
+		/* 		const reponse = await post('http://localhost:1234/accounts/login', {
 			email: 'vimiscool@gmail.com',
 			password: 'emacssucks'
 		});
 
-		setToken(response);
+		setToken(response); */
 	};
 
 	return (
 		<div>
-			<Button onClick={handleClick}>Test</Button>
+			<Button onClick={handleClick}>Testt</Button>
 
 			<p>{token}</p>
 		</div>
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,14 +6,17 @@ module.exports = {
 		open: true,
 		port: 2000
 	},
+	devtool: 'eval-source-map',
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
+				include: path.resolve(__dirname, 'src'),
 				use: ['style-loader', 'css-loader']
 			},
 			{
 				test: /\.js$/,
+				include: path.resolve(__dirname, 'src'),
 				use: ['babel-loader']
 			}
 		]
