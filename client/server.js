86871049d156bae5eed34e4dde1301327446e947
tjuranek <thomas@juranek.com>
2020-12-29 20:04:47 -0600

    test

--- client/server.js
+++ client/server.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import path from path;
+const express = require('express');
+const path = require('path');
 
 const port = 8080;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, 'dist/index.html')));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+	res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port, () => console.log(`Serving Client App on Port ${port}`));
