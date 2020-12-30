5049ffbdd9e2ba6fa6e3beebf7a5ac1ee1646326
tjuranek <thomas@juranek.com>
2020-12-29 20:34:06 -0600

    if this works i will cry

--- client/server.js
+++ client/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 
-const port = 8080;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'index.html'));
+	res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
 app.listen(port, () => console.log(`Serving Client App on Port ${port}`));
