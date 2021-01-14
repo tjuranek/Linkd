65d4f915c77a87cec11f830c61efea34c4151258
tjuranek <thomas@juranek.com>
2021-01-13 21:34:34 -0600

    fix home page formatting

--- client/src/components/marketing-card.jsx
+++ client/src/components/marketing-card.jsx
@@ -1,17 +1,23 @@
-import { Box, Card, CardContent, Typography } from '@material-ui/core';
+import { Card, CardContent, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
+
+const useStyles = makeStyles({
+	card: {
+		height: '100%'
+	}
+});
 
 export const MarketingCard = props => {
 	const { title, description } = props;
 
+	const classes = useStyles();
+
 	return (
-		<Box m={1}>
-			<Card>
-				<CardContent>
-					<Typography variant='h5'>{title}</Typography>
-					<Typography variant='p'>{description}</Typography>
-				</CardContent>
-			</Card>
-		</Box>
+		<Card className={classes.card}>
+			<CardContent>
+				<Typography variant='h5'>{title}</Typography>
+				<Typography variant='p'>{description}</Typography>
+			</CardContent>
+		</Card>
 	);
 };
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,4 +1,4 @@
-import { Grid, makeStyles } from '@material-ui/core';
+import { Box, Grid, makeStyles } from '@material-ui/core';
 import React from 'react';
 import { AddLinkForm } from '../components/add-link-form';
 import { MarketingCard } from '../components/marketing-card';
@@ -8,19 +8,36 @@ import { MainLayout } from './layouts/main-layout';
 export const Home = () => {
 	return (
 		<MainLayout>
-			<Tagline />
-			<AddLinkForm />
-			<Grid container xs={12}>
-				<Grid item xs={12} md={4}>
-					<MarketingCard title='test' description='testing' />
+			<Box mt={5}>
+				<Tagline />
+			</Box>
+
+			<Box mt={3}>
+				<AddLinkForm />
+			</Box>
+
+			<Box mt={5}>
+				<Grid container xs={12} spacing={4}>
+					<Grid item xs={12} md={4}>
+						<MarketingCard
+							title='Share Unlimited Links'
+							description='You have online media, share it. With Linkd you can generate short links for as many urls as you wish.'
+						/>
+					</Grid>
+					<Grid item xs={12} md={4}>
+						<MarketingCard
+							title='Visualize Engagement'
+							description='We give you insight into when and where your users interact with your content.'
+						/>
+					</Grid>
+					<Grid item xs={12} md={4}>
+						<MarketingCard
+							title='Create by Thomas Juranek'
+							description='The not-so-self-proclaimed overthinker delivers yet another side project after deadline and with fewer features.'
+						/>
+					</Grid>
 				</Grid>
-				<Grid item xs={12} md={4}>
-					<MarketingCard title='test' description='testing' />
-				</Grid>
-				<Grid item xs={12} md={4}>
-					<MarketingCard title='test' description='testing' />
-				</Grid>
-			</Grid>
+			</Box>
 		</MainLayout>
 	);
 };
